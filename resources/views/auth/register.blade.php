e19fc6725f5c9a6a3b7a94e199ab62b1d3fcd926
mbembe <mbembesheb@gmail.mail>
2022-06-06 12:22:06 +0200

    modif

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-guest-layout>
+    <x-auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+        <!-- Validation Errors -->
+        <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
-
-<body>
-    <div class="container">
-
-        <form method="POST" action="{{ route('liste') }}">
+        <form method="POST" action="{{ route('register') }}">
             @csrf
-            <div class="container mt-3 text-primary shadow p-3 mb-5 bg-body rounded">
-                <h2 class="text-center mt-3 text-primary shadow p-3 mb-5 bg-body rounded"><strong
-                        style="color: green">FORMULAIRE D'INSCRIPTION</strong></h2>
 
-                <!-- Name -->
-                <div class="mt-4">
-                    <x-label for="name" :value="__('Nom')" />
+            <!-- Name -->
+            <div>
+                <x-label for="name" :value="__('Name')" />
 
-                    <x-input id="name" class="form-control" type="name" name="text" required autocomplete="name" />
-                </div>
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            </div>
 
-                <!-- Email Address -->
-                <div class="mt-4">
-                    <x-label for="email" :value="__('Email')" />
-                    <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required
-                        autofocus />
-                </div>
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-label for="email" :value="__('Email')" />
 
-                <!-- Password -->
-                <div class="mt-4">
-                    <x-label for="password" :value="__('Mot de passe')" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            </div>
 
-                    <x-input id="password" class="form-control" type="password" name="password" required
-                        autocomplete="current-password" />
-                </div>
-                <!-- Confirm Password -->
-                <div class="mt-4">
-                    <x-label for="password_confirmation" :value="__('Confirme le mot de passe')" />
+            <!-- Password -->
+            <div class="mt-4">
+                <x-label for="password" :value="__('Password')" />
 
-                    <x-input id="password_confirmation" class="form-control" type="password"
-                        name="password_confirmation" required />
-                </div>
+                <x-input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="new-password" />
+            </div>
 
-                <div class="flex items-center justify-end mt-4">
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                        {{ __('Already registered?') }}
-                    </a>
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                    <x-button class="ml-4 bg-success">
-                        {{ __('Enregistrer') }}
-                    </x-button>
-                </div>
+                <x-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+
+                <x-button class="ml-4">
+                    {{ __('Register') }}
+                </x-button>
             </div>
         </form>
-    </div>
-</body>
-
-</html>
+    </x-auth-card>
+</x-guest-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,114 +1,72 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Mixte_couture</title>
-
-    <!-- Fonts -->
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-
-    </style>
-</head>
-
-<body class="antialiased">
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-        @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block ">
-                @auth
-                    <a href="{{ url('/dashboard') }}"
-                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
-                        <button type="button" class="btn btn-success">Connexion</button>
-                    </a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}"
-                            class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">
-                            <button type="button" class="btn btn-success">S'inscrire</button>
-                        </a>
-                    @endif
-                @endauth
-            </div>
-        @endif
-    </div>
-    <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Formulaire</title>
+    <title>Document</title>
 </head>
+
 <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.css') }}">
 
 <body>
+ <x-guest-layout>
+        <x-auth-card>
+            <x-slot name="logo">
+                <a href="/">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </x-slot>
 
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-        <div class="container  mt-3 text-primary shadow p-3 mb-5 bg-body rounded">
-            <div class="mt-4">
-                <x-label for="exampleInputEmail1" class="form-label" for="name" :value="__('Nom')" />
+            <!-- Validation Errors -->
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                <x-input id="name" class="form-control" type="name" name="text" required autocomplete="name" />
-            </div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="exampleInputEmail1" class="form-label" for="email" :value="__('Email')" />
+                <!-- Email Address -->
+                <div>
+                    <x-label for="email" :value="__('Email')" />
 
-                <x-input id="email" class="form-control" type="email" name="email" :value="old('email')" required
-                    autofocus />
-            </div>
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                </div>
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="exampleInputEmail1" class="form-label" for="password" :value="__('Mot de passe')" />
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="form-control" type="password" name="password" required
-                    autocomplete="current-password" />
-            </div>
+                    <x-input id="password" class="block mt-1 w-full"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
+                </div>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox"
-                        class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                        name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <!-- Remember Me -->
+                <div class="block mt-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                        href="{{ route('password.request') }}">
-                        {{ __('Mot de passe oublié?') }}
-                    </a>
-                @endif
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
 
-                <x-button class="btn btn-success">
-                    {{ __('Connecter') }}
-                </x-button>
-            </div>
-    </form>
-    </div>
-</body>
-
-</html>
+                    <x-button class="ml-3">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
+            </form>
+        </x-auth-card>
+    </x-guest-layout>
 
 </body>
 </html>
